5c793f242d2a56a6462e601b9684dd8426478dd7
Kacper <krol.kacper.pg2@gmail.com>
2024-04-07 01:57:29 +0200

    Add infrastructure folders and Repository pattern

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,14 +1,51 @@
-﻿
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Identity;
+
+using Infrastructure.Data;
+
 namespace Microsoft.Extensions.DependencyInjection
 {
-
-
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                //options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+
+                options.UseSqlServer(connectionString);
+            });
+
+            //#if (UseApiOnly)
+            //        services.AddAuthentication()
+            //            .AddBearerToken(IdentityConstants.BearerScheme);
+
+            // services.AddAuthorizationBuilder();
+
+            //        services
+            //            .AddIdentityCore<ApplicationUser>()
+            //            .AddRoles<IdentityRole>()
+            //            .AddEntityFrameworkStores<ApplicationDbContext>()
+            //            .AddApiEndpoints();
+            //#else
+            //            services
+            //                .AddDefaultIdentity<ApplicationUser>()
+            //                .AddRoles<IdentityRole>()
+            //                .AddEntityFrameworkStores<ApplicationDbContext>();
+            //#endif
+
+            // services.AddSingleton(TimeProvider.System); services.AddTransient<IIdentityService, IdentityService>();
+
+            // services.AddAuthorization(options => options.AddPolicy(Policies.CanPurge, policy => policy.RequireRole(Roles.Administrator)));
+
             return services;
         }
     }
-
 }
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,10 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
-
+// przeniesienie definiowania scopow do poszczególnych projektów za pomoc¹ depedency injection extension
 builder.Services.AddWebServices();
 builder.Services.AddAppServices();
-builder.Services.AddInfrastructureServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
